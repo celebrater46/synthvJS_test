dea3978e75be36c38a351e78ea04a52e72275020
celebrater <tkgm45nbchk@gmail.com>
2023-03-07 06:56:23 +0900

    edited the code

---
+++ hw.js
@@ -0,0 +1,14 @@
+function getClientInfo() {
+    return {
+      "name" : "Test My Script",
+      "category" : "Example",
+      "author" : "Enin Fujimi",
+      "versionNumber" : 1,
+      "minEditorVersion" : 65540
+    };
+  }
+  
+  function main() {
+    SV.showMessageBox("GREETING", "HELLOOOOO WOOOOOOOORLD!!!!!!!");
+    SV.finish();
+  }
